02b47a9992477a80555012dd1bfe5674e117ae1d
spark1early <bspear.music@gmail.com>
2026-09-04 13:56:24 -0400

    Add files via upload

--- tools/scanner/sw.js
+++ tools/scanner/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 's1el-scanner-v2';
+const CACHE = 's1el-scanner-v3';
 const SHELL = ['./','./index.html','./manifest.webmanifest',
   './icons/icon-192.png','./icons/icon-512.png','./icons/icon-180.png','./icons/icon-32.png'];
 self.addEventListener('install', e => {
